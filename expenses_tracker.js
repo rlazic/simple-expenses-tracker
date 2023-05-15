2b71e665d013cf3df7f4fbb28de081b031e2f10a
rlazic <118766710+rlazic@users.noreply.github.com>
2023-05-15 21:05:06 +0200

    Create expenses_tracker.js

---
+++ expenses_tracker.js
@@ -0,0 +1,52 @@
+// Get DOM elements
+const expenseForm = document.getElementById('expense-form');
+const expenseInput = document.getElementById('expense-input');
+const amountInput = document.getElementById('amount-input');
+const expenseList = document.getElementById('expense-list');
+const totalExpense = document.getElementById('total-expense');
+
+// Initialize expense array
+let expenses = [];
+
+// Add expense to the list
+function addExpense(e) {
+  e.preventDefault();
+  const description = expenseInput.value;
+  const amount = parseFloat(amountInput.value);
+
+  if (description.trim() === '' || isNaN(amount)) {
+    return;
+  }
+
+  const expense = {
+    description,
+    amount
+  };
+
+  expenses.push(expense);
+  expenseInput.value = '';
+  amountInput.value = '';
+
+  updateExpenseList();
+}
+
+// Update the expense list
+function updateExpenseList() {
+  expenseList.innerHTML = '';
+
+  let total = 0;
+  for (let i = 0; i < expenses.length; i++) {
+    const { description, amount } = expenses[i];
+    total += amount;
+
+    const li = document.createElement('li');
+    li.innerHTML = `${description} - $${amount}`;
+    expenseList.appendChild(li);
+  }
+
+  totalExpense.textContent = `Total Expense: $${total.toFixed(2)}`;
+}
+
+// Event listener for adding expense
+expenseForm.addEventListener('submit', addExpense);
+
